80491a59395b9c7ca3de6ec50a2bfcb7d02c73c6
Abdeldjalil-Di-Chougui <abdeldjalil.di.chougui@gmail.com>
2022-05-08 23:23:29 +0100

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -7,24 +7,21 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
       create: (context) => ThemeBloc(),
       child: BlocBuilder <ThemeBloc, ThemeState>(
-          builder: _buildWithTheme
+        builder: _buildWithTheme,
       ),
     );
   }
 
   Widget _buildWithTheme(BuildContext context, ThemeState state) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        theme: state.themeData,
-        home: HomePage()
+      title: 'Flutter Demo',
+      theme: state.themeData,
+      home: HomePage(),
     );
   }
 }
-
